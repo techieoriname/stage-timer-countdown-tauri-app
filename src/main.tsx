e1309d4a190ee3e88fc173e0a4eca73bbc9e9a58
techieoriname <techieoriname@gmail.com>
2024-07-10 11:23:13 +0100

    init toast and refactor main setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import ReactDOM from "react-dom/client";
 import Main from "./screen/Main.tsx";
 import Timer from "./screen/Timer.tsx";
 
-import "./assets/styles/index.css"
+import "./assets/styles/index.css";
+import "react-toastify/dist/ReactToastify.css";
+import 'tippy.js/dist/tippy.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const App = () => {
+    useEffect(() => {
+        // Disable right-click context menu on the entire document
+        const handleContextMenu = (event: Event) => {
+            event.preventDefault();
+        };
+
+        document.addEventListener("contextmenu", handleContextMenu);
+
+        // Clean up event listener when the component unmounts
+        return () => {
+            document.removeEventListener("contextmenu", handleContextMenu);
+        };
+    }, []);
+
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/timer" element={<Timer />} />
+                </Routes>
+            </Router>
+            <ToastContainer theme="dark" />
+        </>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
     <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/timer" element={<Timer />} />
-            </Routes>
-        </Router>
+        <App />
     </React.StrictMode>
 );
+
